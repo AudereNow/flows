03f40796d696c256bfb34a7adc359270d78e04ba
Philip Su <philip@auderenow.org>
2019-09-27 13:09:04 -0700

    Adds CSV upload capability
    The UI looks nasty, and has zero investment in it so far because I anticipate we'll change it.  Just introduces an icon which, when clicked, uses an Input object to upload a CSV to Firebase Storage.
    
    This is a temporary solution, as the backend of the project is coming together.  Uploading to Firebase Storage allows us to next use Storage Triggers (as Cloud Functions) to process each upload into a DB (which is also temporary, until our backend is up and running).

--- src/Components/TopBar.tsx
+++ src/Components/TopBar.tsx
@@ -1,17 +1,76 @@
 import React from "react";
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/storage";
 import "./TopBar.css";
 import logo from "../assets/maishalogo.png";
+import uploadIcon from "../assets/cloud_upload.svg";
+import { UserRole, userRoles } from "../store/corestore";
 
-class TopBar extends React.PureComponent {
+type State = {
+  roles: UserRole[];
+  showFileSelector: boolean;
+};
+
+class TopBar extends React.Component {
+  state: State = {
+    roles: [],
+    showFileSelector: false
+  };
+
+  async componentDidMount() {
+    const roles = await userRoles();
+    this.setState({ roles });
+  }
+
+  _onUploadIconClick = () => {
+    this.setState({ showFileSelector: true });
+  };
+
+  _onFileSelected = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files || event.target.files.length !== 1) {
+      return;
+    }
+
+    const filename = new Date().toISOString() + ".csv";
+    const ref = firebase
+      .storage()
+      .ref()
+      .child(`csvuploads/${filename}`);
+    const file = event.target.files[0];
+
+    try {
+      await ref.put(file, { contentType: file.type });
+
+      alert("File successfully uploaded!");
+      this.setState({ showFileSelector: false });
+    } catch (e) {
+      alert(e);
+    }
+  };
+
   render() {
+    const { roles, showFileSelector } = this.state;
+    const uploadButton =
+      roles.includes(UserRole.AUDITOR) && !showFileSelector ? (
+        <img
+          className="topbar_upload_icon"
+          src={uploadIcon}
+          alt="upload"
+          onClick={this._onUploadIconClick}
+        />
+      ) : null;
+    const uploader = showFileSelector ? (
+      <input type="file" name="file" onChange={this._onFileSelected} />
+    ) : null;
     return (
       <div className="topbar_main">
         <img className="topbar_logo" src={logo} alt="logo" />
         <div className="topbar_user">
           {firebase.auth().currentUser!.displayName}
         </div>
+        {uploadButton}
+        {uploader}
       </div>
     );
   }
