06ce2d92ef54e389b9c3acf3793349a76a7ed82c
Philip Su <philip@auderenow.org>
2019-10-04 17:23:57 -0700

    Show other active users
    In a nod to GDocs, this is a proposal for how we deal with multiple active users working in the same role.  TaskList now writes a little piece of data into Firestore whenever a user clicks on a Task.  Every TaskList subscribes to that collection of active task views, and highlights tasks which others are looking at via an on-hover tooltip.  An orange rectangle highlights any task that someone else is looking at simultaneously (within the last 5 mins).
    
    Note that we don't bother deleting stale ActiveTasks from the Firestore collection.  This is assumed to be ok with our very small user base.  At some later point, we can write some sort of cloud function that cron-jobs itself to remove stale ActiveTasks from the collection.

--- src/Components/TaskList.tsx
+++ src/Components/TaskList.tsx
@@ -1,7 +1,17 @@
 import React from "react";
-import { Task } from "../store/corestore";
+import {
+  Task,
+  ActiveTask,
+  dateFromServerTimestamp,
+  subscribeActiveTasks,
+  logActiveTaskView,
+  getBestUserName
+} from "../store/corestore";
 import LabelWrapper from "./LabelWrapper";
 import "./TaskList.css";
+import ReactTooltip from "react-tooltip";
+
+const MAX_ACTIVE_MSEC = 5 * 60 * 1000; // 5 mins is considered "active"
 
 type Props = {
   tasks: Task[];
@@ -12,12 +22,28 @@ type Props = {
 
 type State = {
   selectedIndex: number | null;
+  activeTasks: ActiveTask[];
 };
 
 class TaskList extends React.Component<Props, State> {
   state: State = {
-    selectedIndex: null
+    selectedIndex: null,
+    activeTasks: []
   };
+  _unsubscribeActives: (() => void) | null = null;
+
+  componentDidMount() {
+    this._unsubscribeActives = subscribeActiveTasks(tasks =>
+      this.setState({ activeTasks: tasks })
+    );
+  }
+
+  componentWillUnmount() {
+    if (this._unsubscribeActives) {
+      this._unsubscribeActives();
+      this._unsubscribeActives = null;
+    }
+  }
 
   _onItemPressed = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const dataName = event.currentTarget.getAttribute("data-name");
@@ -32,20 +58,48 @@ class TaskList extends React.Component<Props, State> {
       this.props.onSelect(index);
     }
 
+    // Let this drift by without await because nothing after it depends on it
+    logActiveTaskView(this.props.tasks[index].id);
+
     this.setState({ selectedIndex: index });
   };
+
+  _isActiveTask(task: Task) {
+    const activeTask = this.state.activeTasks.find(t => t.id === task.id);
+    const recentlyActive =
+      activeTask &&
+      Date.now() - dateFromServerTimestamp(activeTask.since).getTime() <=
+        MAX_ACTIVE_MSEC;
+    if (recentlyActive && activeTask!.name !== getBestUserName()) {
+      return activeTask;
+    }
+    return null;
+  }
 
   render() {
     return (
       <LabelWrapper className={this.props.className} label={"Items to Review"}>
         <div>
           {this.props.tasks.map((task, index) => {
+            const activeTask = this._isActiveTask(task);
+            const activeClass = activeTask ? "tasklist_active" : undefined;
+            const activeDataTip = activeTask
+              ? `${activeTask!.name} also working on this task`
+              : undefined;
+
             return (
-              <div key={index} data-name={index} onClick={this._onItemPressed}>
+              <div
+                className={activeClass}
+                key={index}
+                data-tip={activeDataTip}
+                data-name={index}
+                onClick={this._onItemPressed}
+              >
                 {this.props.renderItem(
                   task,
                   index === this.state.selectedIndex
                 )}
+                <ReactTooltip key={activeDataTip} />
               </div>
             );
           })}
