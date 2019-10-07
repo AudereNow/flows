b36ba9dd929e979196c71126e9129813a78f82e4
Rob <rob@auderenow.org>
2019-10-07 15:21:37 -0700

    Fix search crash, and minor optimization

--- src/util/search.ts
+++ src/util/search.ts
@@ -2,29 +2,29 @@ export const containsSearchTerm = (
   searchPhrase: string,
   entry: any
 ): boolean => {
-  searchPhrase = searchPhrase.trim();
-  if (searchPhrase.length === 0) {
-    return false;
-  }
   const keys = Object.keys(entry);
 
   let searchKey;
   if (searchPhrase.includes(":")) {
     const searchArr = searchPhrase.split(":");
-    searchKey = searchArr[0].trim();
-    searchPhrase = searchArr[1].trim();
+    searchKey = searchArr[0].trim().toLowerCase();
+    searchPhrase = searchArr[1];
+  }
+  searchPhrase = searchPhrase.trim().toLowerCase();
+  if (searchPhrase.length === 0) {
+    return false;
   }
   for (let i = 0; i < keys.length; i++) {
-    const value = entry[keys[i]].trim();
+    const value = entry[keys[i]];
     if (
-      ((!!searchKey &&
-        keys[i].toLowerCase().includes(searchKey.toLowerCase())) ||
+      ((!!searchKey && keys[i].toLowerCase().includes(searchKey)) ||
         !searchKey) &&
       !!value &&
       value
         .toString()
+        .trim()
         .toLowerCase()
-        .includes(searchPhrase.toLowerCase())
+        .includes(searchPhrase)
     ) {
       return true;
     }
