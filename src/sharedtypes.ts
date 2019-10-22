11f75b22cf4fe7538d1466409e8db24b59732857
Philip Su <philip@auderenow.org>
2019-10-22 11:16:15 -0700

    Removed old collection references

--- src/sharedtypes.ts
+++ src/sharedtypes.ts
@@ -6,17 +6,12 @@
   You should only put things in here that don't have module dependencies.
   Simple types, simple functions.
 */
-export const AUDITOR_TASK_COLLECTION = "auditor_task";
-export const OPERATOR_TASK_COLLECTION = "operator_task";
-export const PAYOR_TASK_COLLECTION = "payor_task";
-export const PAYMENT_COMPLETE_TASK_COLLECTION = "payment_complete_task";
 export const ACTIVE_TASK_COLLECTION = "actively_viewed_tasks";
-export const REJECTED_TASK_COLLECTION = "rejected_task";
 export const ADMIN_LOG_EVENT_COLLECTION = "admin_log_event";
 export const TASK_CHANGE_COLLECTION = "task_changes";
 export const METADATA_COLLECTION = "metadata";
-
 export const TASKS_COLLECTION = "tasks";
+
 export enum TaskState {
   CSV = "CSV",
   AUDIT = "AUDIT",
