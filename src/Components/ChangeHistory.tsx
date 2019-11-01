c0ffa1ffc5f630e3b5337f37e1faf83b787ed654
Philip Su <philip@auderenow.org>
2019-10-31 17:16:29 -0700

    Much better TaskChangeRecord rendering
    We integrate `react-table`, which supports column widths, paging, sorting, and a bunch of other things automatically.

--- src/Components/ChangeHistory.tsx
+++ src/Components/ChangeHistory.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import { TaskChangeRecord } from "../sharedtypes";
 import { getAllChanges } from "../store/corestore";
-import DataTable from "./DataTable";
+import ReactTooltip from "react-tooltip";
+import moment from "moment";
+import ReactTable, { RowRenderProps } from "react-table";
+import "react-table/react-table.css";
+
+const TABLE_COLUMNS = [
+  { Header: "Task ID", accessor: "taskID", minWidth: 150 },
+  {
+    Header: "Time",
+    accessor: "timestamp",
+    Cell: (props: RowRenderProps) => renderTooltippedTime(props.value),
+    minWidth: 100
+  },
+  { Header: "Description", accessor: "description", minWidth: 450 },
+  { Header: "Notes", accessor: "notes", minWidth: 200 }
+];
+
+type ChangeRow = {
+  taskID: string;
+  timestamp: number;
+  description: string;
+  notes?: string;
+};
 
 type Props = {};
 type State = {
-  changes: TaskChangeRecord[];
+  changes: ChangeRow[];
 };
 
 class ChangeHistory extends React.Component<Props, State> {
@@ -15,7 +37,19 @@ class ChangeHistory extends React.Component<Props, State> {
 
   async componentDidMount() {
     const changes = await getAllChanges();
-    this.setState({ changes });
+
+    this.setState({ changes: this._recordsToChangeLogs(changes) });
+  }
+
+  _recordsToChangeLogs(records: TaskChangeRecord[]): ChangeRow[] {
+    return records.map(r => {
+      return {
+        taskID: r.taskID,
+        timestamp: r.timestamp,
+        description: `${r.by} changed task from ${r.fromState} to ${r.state}`,
+        notes: r.notes
+      };
+    });
   }
 
   render() {
@@ -25,11 +59,27 @@ class ChangeHistory extends React.Component<Props, State> {
         {changes.length === 0 ? (
           "No changes found"
         ) : (
-          <DataTable data={changes} />
+          <ReactTable
+            data={changes}
+            columns={TABLE_COLUMNS}
+            defaultPageSize={50}
+          />
         )}
       </div>
     );
   }
+}
+
+function renderTooltippedTime(timestamp: number) {
+  const when = moment(timestamp).fromNow();
+  const tip = new Date(timestamp).toLocaleString();
+
+  return (
+    <span data-tip={tip}>
+      {when}
+      <ReactTooltip key={tip} />
+    </span>
+  );
 }
 
 export default ChangeHistory;
