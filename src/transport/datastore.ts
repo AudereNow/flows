a19dcdfe1f7fbfe6b8f0598b38f4640f872e11c8
Ram Kandasamy <ram@auderenow.org>
2020-10-18 22:03:26 -0700

    Use firebase SDK auto-configuration
    This avoids having to manage firebaseconfig.json files, and simplifies
    setup for non-firebase mode.

--- src/transport/datastore.ts
+++ src/transport/datastore.ts
@@ -6,11 +6,13 @@ import { RestDataStore } from "./maishaDatastore";
 
 export let dataStore: DataStore;
 
-export function initializeStore(config: DataStoreConfig): DataStore {
+export async function initializeStore(
+  config: DataStoreConfig
+): Promise<DataStore> {
   switch (config.type) {
     case DataStoreType.FIREBASE:
       const firebaseDataStore = new FirebaseDataStore();
-      firebaseDataStore.initializeStore();
+      await firebaseDataStore.initializeStore();
       dataStore = firebaseDataStore;
       return dataStore;
     case DataStoreType.REST:
